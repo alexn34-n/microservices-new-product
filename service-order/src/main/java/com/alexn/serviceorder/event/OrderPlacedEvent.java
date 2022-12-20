b91a8b94215f1067df2bf4c3e4c71aaa89923ba4
alexn <alexn3469@mail.ru>
2022-12-20 21:09:32 +0300

    add kafka

---
+++ service-order/src/main/java/com/alexn/serviceorder/event/OrderPlacedEvent.java
@@ -0,0 +1,14 @@
+package com.alexn.serviceorder.event;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderPlacedEvent {
+    private String orderNumber;
+
+}
--- service-order/src/main/java/com/alexn/serviceorder/service/OrderService.java
+++ service-order/src/main/java/com/alexn/serviceorder/service/OrderService.java
@@ -6,10 +6,12 @@ import brave.Tracer;
 import com.alexn.serviceorder.dto.InventoryResponse;
 import com.alexn.serviceorder.dto.OrderLineItemsDto;
 import com.alexn.serviceorder.dto.OrderRequest;
+import com.alexn.serviceorder.event.OrderPlacedEvent;
 import com.alexn.serviceorder.model.Order;
 import com.alexn.serviceorder.model.OrderLineItems;
 import com.alexn.serviceorder.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -26,6 +28,7 @@ public class OrderService {
     private final OrderRepository orderRepository;
     private  final WebClient.Builder webClientBuilder;
     private  final Tracer tracer;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
     public String placeOrder(OrderRequest orderRequest){
         Order order = new Order();
@@ -61,6 +64,7 @@ public class OrderService {
 
             if(allProductsInStock){
                 orderRepository.save(order);
+                kafkaTemplate.send("notificationTopic", new OrderPlacedEvent());
                 return "Order Placed Successfully";
             } else {
                 throw new IllegalArgumentException("Product is not in stock, please try again later");
